01322da708d12dbf3c9587802a610a9b12c4cec7
Larry Ruckman <ruckman@slac.stanford.edu>
2019-11-19 16:50:55 -0800

    bug fix

--- firmware/targets/RceDpm/DpmIbertTester/ruckus.tcl
+++ firmware/targets/RceDpm/DpmIbertTester/ruckus.tcl
@@ -4,7 +4,6 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 # Load common and sub-module ruckus.tcl files
 loadRuckusTcl $::env(TOP_DIR)/submodules/surf
 loadRuckusTcl $::env(TOP_DIR)/submodules/rce-gen3-fw-lib/DpmCore
-loadRuckusTcl $::env(TOP_DIR)/submodules/xvc-udp-debug-bridge
 
 # Load local Source Code and constraints
 loadSource -dir       "$::DIR_PATH/hdl"
