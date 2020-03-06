b533039dc1b6f21f5d18c6840027c101d19116bf
Larry Ruckman <ruckman@slac.stanford.edu>
2020-03-06 15:13:30 -0800

    preparing for release builds

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,7 +1,7 @@
 export TIG_PULSE = 1
 
-# Define Firmware Version: v1.0.4.0
-export PRJ_VERSION = 0x01000400
+# Define Firmware Version: v1.1.0.0
+export PRJ_VERSION = 0x01010000
 
 # Define release
 ifndef RELEASE
