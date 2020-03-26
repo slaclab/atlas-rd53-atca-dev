95ef85a325fc0c44063530c5142334b7180c212e
Larry Ruckman <ruckman@slac.stanford.edu>
2020-03-26 11:34:13 -0700

    updating FW version

--- firmware/targets/shared_config.mk
+++ firmware/targets/shared_config.mk
@@ -1,7 +1,7 @@
 export TIG_PULSE = 1
 
-# Define Firmware Version: v1.1.0.6
-export PRJ_VERSION = 0x01010006
+# Define Firmware Version: v1.1.0.8
+export PRJ_VERSION = 0x01010008
 
 # Define release
 ifndef RELEASE
